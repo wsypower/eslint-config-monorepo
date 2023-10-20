6e9b76447988a7e8c3abd34ba8c0fa6dc077f27a
weiyafei <2553241022@qq.com>
2023-10-20 16:23:30 +0800

    feat(plugin): max-attributes-pre-line 多行添加测试文件

--- packages/eslint-plugin/src/test/max-attributes-pre-line.spec.ts
+++ packages/eslint-plugin/src/test/max-attributes-pre-line.spec.ts
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: wsy
  * @Date: 2023-10-19 16:31:31
- * @LastEditTime: 2023-10-19 19:29:56
+ * @LastEditTime: 2023-10-20 16:22:03
  * @LastEditors: wsy
  */
 import { RuleTester } from '@typescript-eslint/rule-tester'
@@ -52,6 +52,28 @@ const valids = [
     </template>`,
     options: [{ singleline: { max: 1 } }],
   },
+  {
+    code: `
+    <template>
+      <div>
+        <a
+          href="https://vitejs.dev"
+          target="_blank" foo="1" />
+      </div>
+    </template>`,
+    options: [{ singleline: { max: 3 }, multiline: { max: 2 } }],
+  },
+  {
+    code: `
+    <template>
+      <div>
+        <a
+          href="https://vitejs.dev"
+          target="_blank" foo="1" a="ceshi" />
+      </div>
+    </template>`,
+    options: [{ singleline: { max: 3 }, multiline: { max: 3 } }],
+  },
 ]
 
 const invalids = [
@@ -77,6 +99,66 @@ target="_blank" />
       },
       {
         message: '\'target\' should be on a new line.',
+      },
+    ],
+  },
+  {
+    code: `
+      <template>
+        <div>
+          <a
+href="https://vitejs.dev" foo="1"
+target="_blank" />
+        </div>
+      </template>`,
+    options: [{ singleline: { max: 1 }, multiline: { max: 1 } }],
+    output: `
+      <template>
+        <div>
+          <a
+href="https://vitejs.dev"
+foo="1"
+target="_blank" />
+        </div>
+      </template>`,
+    errors: [
+      {
+        message: '\'href\' should be on a new line.',
+      },
+      {
+        message: '\'foo\' should be on a new line.',
+      },
+    ],
+  },
+  {
+    code: `
+      <template>
+        <div>
+          <a
+href="https://vitejs.dev" foo="1" foo2="2"
+target="_blank" />
+        </div>
+      </template>`,
+    options: [{ singleline: { max: 1 }, multiline: { max: 2 } }],
+    output: `
+      <template>
+        <div>
+          <a
+href="https://vitejs.dev"
+foo="1"
+foo2="2"
+target="_blank" />
+        </div>
+      </template>`,
+    errors: [
+      {
+        message: '\'href\' should be on a new line.',
+      },
+      {
+        message: '\'foo\' should be on a new line.',
+      },
+      {
+        message: '\'foo2\' should be on a new line.',
       },
     ],
   },
